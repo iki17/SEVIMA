1166df73ade9f7e9ffceb4677549919e8e38f7c1
iki17 <mrizkijokam354@gmail.com>
2022-06-25 15:19:05 +0700

    menambah fitur tambah murid

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const { application } = require('express')
-const express = require('express')
-const mysql = require ('mysql')
+const { application } = require("express")
+const express = require("express")
+const mysql = require ("mysql")
+const BodyParser = require("body-parser")
 
 const app = express ();
+
+app.use(BodyParser.urlencoded({extended: true}))
 
 app.set("view engine", "ejs")
 app.set("views", "views" )
@@ -23,9 +26,22 @@ db.connect((err) => {
     db.query(sql, (err, result)=> {
         const users = JSON.parse(JSON.stringify(result))
         console.log("hasil database ->", users)
+
+        // untuk get data
         app.get("/", (req, res) => {
-            res.render("index" , {users: users, title : "rizki "})
+            res.render("index" , {users: users, title : "DAFTAR MURID BARU "})
         })
+        // untuk insert data
+        app.post("/tambah", (req, res) => {
+            const insertsql = `INSERT INTO murid_sd (nama,umur,nama_ibu) VALUES ('${req.body.nama}',
+            '${req.body.umur}','${req.body.nama_ibu}');`
+            db.query(insertsql, (err,result)=> {
+
+                if(err) throw err
+                res.redirect("/");
+            })
+        })
+
     })
 }) 
 
