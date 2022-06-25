a881c6d2dbca5c2d3a0371c1925b0777319a4f41
iki17 <mrizkijokam354@gmail.com>
2022-06-25 21:12:35 +0700

    memperbaiki daftar murid

--- server.js
+++ server.js
@@ -22,27 +22,28 @@ db.connect((err) => {
     if(err) throw err
     console.log('database conected')
 
-    const sql= "SELECT * FROM `murid_sd`"
-    db.query(sql, (err, result)=> {
-        const users = JSON.parse(JSON.stringify(result))
-        console.log("hasil database ->", users)
+ 
 
-        // untuk get data
-        app.get("/", (req, res) => {
+            // untuk get data
+    app.get("/", (req, res) => {
+        const sql= "SELECT * FROM `murid_sd`"
+        db.query(sql, (err, result)=> {
+            const users = JSON.parse(JSON.stringify(result))
             res.render("index" , {users: users, title : "DAFTAR MURID BARU "})
-        })
-        // untuk insert data
-        app.post("/tambah", (req, res) => {
-            const insertsql = `INSERT INTO murid_sd (nama,umur,nama_ibu) VALUES ('${req.body.nama}',
-            '${req.body.umur}','${req.body.nama_ibu}');`
-            db.query(insertsql, (err,result)=> {
-
-                if(err) throw err
-                res.redirect("/");
-            })
-        })
-
+         })    
     })
+
+            // untuk insert data
+            app.post("/", (req, res) => {
+                const insertsql = `INSERT INTO murid_sd (nama,no_nisn,umur,nama_ibu) VALUES ('${req.body.nama}',
+                '${req.body.no_nisn}','${req.body.umur}','${req.body.nama_ibu}');`
+                db.query(insertsql, (err,result)=> {
+    
+                    if(err) throw err
+                    res.redirect("/");
+                })
+            })
+
 }) 
 
 
